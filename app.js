4450f3f3a450b3de1d129fb1d3823bdfa17bb11c
BekirLee <nihadbekirli8@gmail.com>
2024-01-15 16:34:34 +0400

    codes for filter side

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 const filterContainer = document.querySelector(".filters");
 const allProducts = document.querySelector('.products');
 
+getProducts()
+filterCategories()
+getCategory();
+
+
 function filterCategories() {
     fetch('https://fakestoreapi.com/products/categories')
         .then(res => res.json())
@@ -9,7 +14,7 @@ function filterCategories() {
             console.log(categories)
             categories.forEach((category) => {
                 filterContainer.innerHTML += `
-                <button class="filterBtn">
+                <button class="filterBtn" onclick="getCategory(this)">
                     <span>${category}</span>
                 </button>
                 `;
@@ -45,7 +50,34 @@ function getProducts() {
         ))
 }
 
-getProducts()
-filterCategories()
 
+
+function getCategory(categoryProducts) {
+    let getCategories = categoryProducts.textContent.trim();
+    // console.log(categoryProducts.innerHTML.trim())
+    fetch(`https://fakestoreapi.com/products/category/${getCategories}`)
+        .then(res => res.json())
+        .then(products => {
+            allProducts.innerHTML = ' ';
+            products.forEach(product => {
+
+                const { image, title, price } = product;
+                console.log(allProducts);
+                allProducts.innerHTML += `
+                    <div class="productItem">
+                    <div class="productPhoto">
+                        <img src="${image}" alt="">
+                    </div>
+    
+                    <div class="product-info">
+                        <h5 class='productTitle' >${title.slice(0, 20).concat('...')}</h5>
+                        <h6 class='productPrice' >$ ${price}</h6>
+                        <button>Add to cart</button>
+                    </div>
+                </div>
+    
+                    `
+            })
+        })
+}
 // console.log('hello')
