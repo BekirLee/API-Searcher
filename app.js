a9237b8d0535d7094ddba36a7b86f5504da8fe63
BekirLee <nihadbekirli8@gmail.com>
2024-01-15 17:27:15 +0400

    codes for input side

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 // filterig categories
 const filterContainer = document.querySelector(".filters");
 const allProducts = document.querySelector('.products');
+// let searchInput = document.querySelector('.searchInput');
+let searchInput = document.querySelector(".searchInput")
 
+
+// search()
 getProducts()
 filterCategories()
 getCategory();
+
+
+
+// function search() {
+
+//     searchInput.addEventListener('keyup', (e) => {
+//         e.preventDefault();
+//         console.log(e.target.value);
+
+//     })
+// }
 
 
 function filterCategories() {
@@ -25,12 +40,26 @@ function filterCategories() {
 }
 
 function getProducts() {
+    // search
+    searchInput.addEventListener('keyup', (e) => {
+        e.preventDefault();
+        // console.log(e.target.value);
+        var inputs = e.target.value;
+    })
+
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
         .then(products => (
             products.forEach(product => {
+
                 console.log(product);
                 const { image, title, price } = product;
+
+                title.filter((searched) => {
+                    if (searched == inputs) {
+                        inputs.style.color = "orange"
+                    }
+                })
 
                 allProducts.innerHTML += `
                 <div class="productItem">
@@ -41,7 +70,7 @@ function getProducts() {
                 <div class="product-info">
                     <h5 class='productTitle' >${title.slice(0, 20).concat('...')}</h5>
                     <h6 class='productPrice' >$ ${price}</h6>
-                    <button>Add to cart</button>
+                        <button>Add to cart</button>
                 </div>
             </div>
                 `
@@ -55,8 +84,8 @@ function getCategory(categoryProducts) {
     let getCategories = categoryProducts.textContent.trim();
     // console.log(categoryProducts.innerHTML.trim())
     if (getCategories == 'All') {
-        // getCategories = '/';
-        console.log(getCategories)
+        getCategories = '/';
+        // console.log(getCategories)
     }
     else {
         getCategories = `category/${getCategories}`;
@@ -87,4 +116,3 @@ function getCategory(categoryProducts) {
             })
         })
 }
-// console.log('hello')
